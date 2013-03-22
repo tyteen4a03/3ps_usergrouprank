8fc6ae676d03689f7321e204f9788849b7be8707
tyteen4a03 <tyteen4a03@3pointstudio.com>
2013-03-22 15:21:48 +0800

    Update installer

--- ThreePointStudio/UsergroupRanks/Installer.php
+++ ThreePointStudio/UsergroupRanks/Installer.php
@@ -6,28 +6,26 @@
 */
 
 class ThreePointStudio_UsergroupRanks_Installer {
-	var $dbQueries = array(
-		'createQuery' => 'CREATE TABLE IF NOT EXISTS `3ps_usergroup_ranks` (
-  `rid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `rank_type` mediumint(8) unsigned NOT NULL,
-  `rank_usergroup` text CHARACTER SET latin1 NOT NULL,
-  `rank_active` tinyint(1) unsigned NOT NULL,
-  `rank_content` text COLLATE utf8_unicode_ci NOT NULL,
-  `rank_display_condition` tinyint(3) unsigned NOT NULL,
-  `rank_style_priority_limit` int(10) unsigned NOT NULL,
-  UNIQUE KEY `rid` (`rid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;',
-		'dropQuery' => 'DROP TABLE IF EXISTS `3ps_usergroup_ranks`',
-	);
-
-	protected static function install() {
-		$db = XenForo_Application::get('db');
-		$db->query(self::$dbQueries['createQuery']);
+	public static final function install($installedAddon) {
+		$db = XenForo_Application::getDb();
+		$version = is_array($installedAddon) ? $installedAddon['version_id'] : 0;
+		if ($version < 1) {
+			$db->query('CREATE TABLE IF NOT EXISTS `3ps_usergroup_ranks` (
+						  `rid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+						  `rank_type` mediumint(8) unsigned NOT NULL,
+						  `rank_usergroup` text CHARACTER SET latin1 NOT NULL,
+						  `rank_active` tinyint(1) unsigned NOT NULL,
+						  `rank_content` text COLLATE utf8_unicode_ci NOT NULL,
+						  `rank_display_condition` tinyint(3) unsigned NOT NULL,
+						  `rank_style_priority_limit` int(10) unsigned NOT NULL,
+						  UNIQUE KEY `rid` (`rid`)
+						) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;');
+		}
 	}
 
-	protected static function uninstall() {
-		$db = XenForo_Application::get('db');
-		$db->query(self::$dbQueries['dropQuery']);
+	public static final function uninstall() {
+		$db = XenForo_Application::getDb();
+		$db->query("DROP TABLE IF EXISTS `3ps_usergroup_ranks`");
 	}
 }
 
