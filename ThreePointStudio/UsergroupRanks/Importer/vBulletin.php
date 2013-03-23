e13fe7308f663422102d85540d0344e9ad16c1b1
tyteen4a03 <tyteen4a03@3pointstudio.com>
2013-03-23 14:01:23 +0800

    1.1.0 tag -Obsolete methods removed, some methods are renamed -Added display rank base on posts condition -Started work on importers

--- ThreePointStudio/UsergroupRanks/Importer/vBulletin.php
+++ ThreePointStudio/UsergroupRanks/Importer/vBulletin.php
@@ -1,4 +1,9 @@
 <?php
+/*
+* Usergroup Ranks v1.1.0 written by tyteen4a03@3.studIo.
+* This software is licensed under the BSD 2-Clause modified License.
+* See the LICENSE file within the package for details.
+*/
 
 class ThreePointStudio_UsergroupRanks_Importer_vBulletin extends XFCP_ThreePointStudio_UsergroupRanks_Importer_vBulletin {
 	public function getSteps() {
